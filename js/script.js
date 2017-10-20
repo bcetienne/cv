d9d198f680e2c8127187365d32a97fb97e1f0f93
admin <admin@iMac3.local>
2017-10-20 16:56:10 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
-	var words = ["un curieux", "un passionné", "dynamique", "un gars sympa", "un savoyard"],
-		i = 0;
-
+	
+	/**
+	 * [description]
+	 * @param  {Array}  ){		var words         [Liste des mots qui seront affichés]
+	 * @param  {[integer]} 3500     [Temps en millisecondes]
+	 */
 	//Changement de la phrase d'accroche (toutes les 3.5 secondes)
 	setInterval(function(){
+		var words = ["un curieux", "un développeur", "un passionné", "un savoyard"],
+		i = 0;
 		//Effacement du mot à l'id "word"
 		$("#word").fadeOut(function() {
 			//Affichage aléatoire des mots sur l'id précédemment éffacé
@@ -11,4 +16,15 @@ $(document).ready(function() {
 		});
 		//Temps en millisecondes
 	}, 3500);
+
+
+	$(".scroll").on("click", function(){
+		var target = $(this).attr("href"),
+			speed = 750;
+
+			$("html", "body").animate({
+				scrollTop: $(target).offset().top
+			}, speed);
+			return false;
+	});
 });
